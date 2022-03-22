1759acde8698ba39fc50075b5cad9f7251d4f4eb
diegopuma <diegopumacode@gmail.com>
2022-03-22 09:27:55 -0500

    adding layout main page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,14 +3,19 @@ import { Outlet } from 'react-router-dom'
 import Content from '../../components/Content'
 import Navbar from '../../components/navbar'
 import Trends from '../../components/trends'
+import { Box } from '@chakra-ui/react'
 
 export default function Main() {
     return (
         <>
             <Navbar />
-            <Content marginTop={5}>
-                <Outlet />
-                <Trends/>
+            <Content marginTop={5} display='flex' gap={'3rem'}>
+                <Box flex={[1, 1, 3]}>
+                    <Outlet />
+                </Box>
+                <Box flex={1} display={["none", "none", "block"]}>
+                    <Trends />
+                </Box>
             </Content>
         </>
     )
